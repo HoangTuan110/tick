c2d1f4c191ef465ea167dc0c03ada082eea2a831
Dang Hoang Tuan <danghoangtuan526@proton.me>
2023-01-30 16:10:46 +0700

    (index.js; checkbox.js) Migrate all of the checkbox-related code (which is also the most important ones) to checkbox.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 import m from "mithril"
+import { generateCheckboxes, generateButtonCheckboxDifficulty, countTickedCheckboxes } from "./checkbox"
 import { inject } from '@vercel/analytics';
 
 inject();
 
 // == Constants ==
 const r = document.body
-const NUMBER_CHECKBOXES_IN_ONE_ROW = 10
-// Checkbox difficulty (aka the amount of checkboxes for each difficulty)
-const CHECKBOX_DIFF = [10, 20, 50, 100, 250, 500, 1000]
 
 // == State ==
 var state = {
@@ -40,74 +38,6 @@ var timer = {
     if (timer.startTime)
       return timer.msToTime(Date.now() - timer.startTime)
   },
-}
-
-// Reset everything
-const reset = () => {
-  timer.startTime = undefined
-  state.checkbox_diff = 0 // Doesn't really needed, since it got changed after changing diff anyways
-}
-
-// Display the number with random sign
-// https://checkboxrace.com/script.js
-const randomPosOrNeg = (number) => {
-  const posOrNeg = Math.random() < 0.5 ? -1 : 1
-  return Math.min(Math.random() * number, window.innerWidth - 500) * posOrNeg;
-}
-
-// Generate checkboxes and the state for the difficulty given
-const generateCheckboxes = (num) => {
-  if (num === 0) return
-  // Generate a checkbox and pushed it to the checkbox element list
-  const generateCheckbox = i => {
-    state.checkbox_lst.push(
-      m("input", {
-        "type": "checkbox",
-        "class": `checkbox`,
-        "style": `transform: translateX(${randomPosOrNeg(i)}px)`,
-        "onclick": () => {
-          state.checkboxes[i] = !state.checkboxes[i]
-          //console.log(`Checkbox number ${i} has been ticked!`)
-        }
-      })
-    )
-  }
-  // Reset the state checkbox array and the checkbox list when 
-  // calling the function to switch to another difficulty without
-  // leftover ticks
-  state.checkboxes = []
-  state.checkbox_lst = state.checkbox_lst.fill(null)
-  // Generate the checkbox elements
-  for (let i = 0; i < num; i++) {
-    if (i !== 0 && i % NUMBER_CHECKBOXES_IN_ONE_ROW == 0) {
-      state.checkbox_lst.push(m("br"))
-      generateCheckbox(i)
-    }
-    else generateCheckbox(i)
-  }
-  // Filter out nulls in the state array
-  state.checkboxes = state.checkboxes.filter(n => n)
-  //console.log(`Generated ${num} checkboxes`)
-}
-
-// Generate buttons that indicate checkbox difficulty
-const generateButtonCheckboxDifficulty = () => {
-  return CHECKBOX_DIFF.map(
-    x => m("button", {
-      "onclick": () => {
-        reset()
-        state.checkbox_diff = x
-        generateCheckboxes(state.checkbox_diff)
-      },
-      "class": `diff-btn`
-    }, x)
-  )
-}
-
-// Count the number of ticked checkboxes
-const countTickedCheckboxes = () => {
-  console.log(state.checkboxes)
-  return state.checkboxes.filter(x => x).length
 }
 
 // Results section
