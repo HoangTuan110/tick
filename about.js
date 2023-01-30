adb48a60e9e5859ecbae2bd3a690257995830e51
Dang Hoang Tuan <danghoangtuan526@proton.me>
2023-01-30 17:02:06 +0700

    (about.js; credits.js) Adding boilerplate for Credits and About sections

---
+++ about.js
@@ -0,0 +1,13 @@
+import m from "mithril"
+
+const About = () => {
+  return {
+    view: () => {
+      return m("main", [
+        m("b.header", "About")
+      ])
+    }
+  }
+}
+
+export default About
---
+++ credits.js
@@ -0,0 +1,13 @@
+import m from "mithril"
+
+const Credits = () => {
+  return {
+    view: () => {
+      return m("main", [
+        m("b.header", "Credits")
+      ])
+    }
+  }
+}
+
+export default Credits
